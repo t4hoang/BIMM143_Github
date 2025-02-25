0cd60403e3e6e4a562813f13a68caa370d08701b
Trevor Hoang <t4hoang@ucsd.edu>
2025-02-25 11:37:18 -0800

    Commiting class files

---
+++ Lab 4/Lab-4_files/Lab 4.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Lab 4:Intro to R"
+#' author: "Trevor Hoang"
+#' date: "Jan 16, 2025"
+#' ---
+
+
+x=1:50
+plot(x, sin(x))
+
+#Make this nicer
+plot(x, sin(x), type="line", col="blue", lwd=3)
